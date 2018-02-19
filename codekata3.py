36776508a9ad1eb89e08898dc9ca2a5073b2f246
SKSMKHari <hariprasathks6@gmail.com>
2018-02-19 17:16:06 +0530

    Update Codekata3.py

--- codekata3.py
+++ codekata3.py
@@ -1,1 +1,8 @@
-alpha=(input("Enter a Name:"))
+alpha = input("Type an alphabet: ")
+
+if alpha in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % l)
+elif alpha == 'y':
+	print("May be a Vowel based on Usage")
+else:
+	print("%s is a consonant." % l) 
