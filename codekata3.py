5b02fb0b9926666465a2f612da6d1cf49661eb28
SKSMKHari <hariprasathks6@gmail.com>
2018-02-19 17:18:07 +0530

    Update Codekata3.py

--- codekata3.py
+++ codekata3.py
@@ -1,8 +1,8 @@
 alpha = input("Type an alphabet: ")
 
 if alpha in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % l)
+	print("%s is a vowel." % alpha)
 elif alpha == 'y':
 	print("May be a Vowel based on Usage")
 else:
-	print("%s is a consonant." % l) 
+	print("%s is a consonant." % alpha) 
