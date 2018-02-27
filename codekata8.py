c50a3de172f1f1b8503578d4456672f58d37f186
SKSMKHari <hariprasathks6@gmail.com>
2018-02-27 17:59:25 +0530

    Update codekata8.py

--- codekata8.py
+++ codekata8.py
@@ -1,6 +1,7 @@
-n=int(input("Enter input: "))
+n=int(input("Enter a number: "))
+n1=n
 sum1 = 0
 while(n > 0):
     sum1=sum1+n
     n=n-1
-print("The sum of first n natural numbers is",sum1)
+print("The sum of first %d natural numbers is"%(n1),sum1)
