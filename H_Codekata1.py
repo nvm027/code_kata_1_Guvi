c9f5ac2a59c41a48643dbe25dea47b6c0127c573
SKSMKHari <hariprasathks6@gmail.com>
2018-03-06 17:47:53 +0530

    Create H_Codekata1.py

---
+++ H_Codekata1.py
@@ -0,0 +1,14 @@
+arr = []
+ipt=int(input("Enter No. of Elements:"))
+for i in range(1,ipt+1):
+    arr.append(int(input("Enter the %s number: ")))
+arr_size = len(arr)
+def printRepeating(arr,size) :
+	count = [0] * size
+	print(" Repeating elements are ",end = "")
+	for i in range(0, size) :
+		if(count[arr[i]] == 1) :
+			print(arr[i], end = " ")
+		else :
+			count[arr[i]] = count[arr[i]] + 1
+printRepeating(arr, arr_size)
